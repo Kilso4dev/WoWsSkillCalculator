e8847d4b314eefc92bd701f33e7949eea7b7808c
Robert Reuter <denib.qatlh@t-online.de>
2017-04-30 22:02:09 +0200

    Initial Commit 30.04. 22:00

--- src/de/Kilso4dev/wowsSkillCalculator/windowComponents/TechTreePanel.java
+++ src/de/Kilso4dev/wowsSkillCalculator/windowComponents/TechTreePanel.java
@@ -9,156 +9,187 @@ class TechTreePanel extends JPanel {
     private final Font fAll = new Font("Times New Roman", Font.BOLD, 20);
 
     TechTreePanel(String nation){
-        this.nation = nation;
+        this(nation, );
+    }
+    
+    TechTreePanel(String nation, JComponent src) {
+        setNation(nation);
         this.setLayout(new GridLayout(11, 10, 30, 50));
 
         if (nation.equals("Arpeggio")) {
-            createArpeggio();
+            createArpeggio(src);
         } else if (nation.equals("Japan")) {
-            createJapan();
+            createJapan(src);
         } else if (nation.equals("USA")) {
-            createUSA();
+            createUSA(src);
         } else if (nation.equals("USSR")) {
-            createUSSR();
+            createUSSR(src);
         } else if (nation.equals("KM")) {
-            createKM();
+            createKM(src);
         } else if (nation.equals("UK")) {
-            createUK();
+            createUK(src);
         } else if (nation.equals("Poland")) {
-            createPoland();
+            createPoland(src);
         } else if (nation.equals("Asia")) {
-            createAsia();
+            createAsia(src);
         } else if (nation.equals("France")) {
-            createFrance();
+            createFrance(src);
         } else if (nation.equals("Italy")) {
-            createItaly();
+            createItaly(src);
         } else if (nation.equals("Commonwealth")) {
-            createCommonwealth();
+            createCommonwealth(src);
         }
         for (Component cComponent : this.getComponents()) {
             cComponent.setFont(fAll);
         }
     }
+        
 
-    private void createArpeggio() {
-        this.add(new JLabel(""));
-        createMultipleLabels(4, "Premium");
-        createMultipleLabels(5, "");
-        this.add(new JLabel("Tier I"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier II"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier III"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier IV"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier V"));
-        this.add(new ClickLabel("ARP Kongo"));
-        this.add(new ClickLabel("ARP Kirishima"));
-        this.add(new ClickLabel("ARP Haruna"));
-        this.add(new ClickLabel("ARP Hiei"));
-        createMultipleLabels(5, "");
-        this.add(new JLabel("Tier VI"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier VII"));
-        this.add(new ClickLabel("ARP Myoko"));
-        this.add(new ClickLabel("ARP Haguro"));
-        this.add(new ClickLabel("ARP Ashigara"));
-        this.add(new ClickLabel("ARP Nachi"));
-        createMultipleLabels(5, "");
-        this.add(new JLabel("Tier VIII"));
-        this.add(new ClickLabel("ARP Takao"));
-        createMultipleLabels(8, "");
-        this.add(new JLabel("Tier IX"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier X"));
+    private void createArpeggio(JComponent src) {
+        src.add(new JLabel("ARP"));
+        createMultipleLabels(src, 4, "Premium");
+        createMultipleLabels(src, 5, "");
+
+        src.add(new JLabel("Tier I"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier II"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier III"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier IV"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier V"));
+        src.add(new ClickLabel("ARP Kongo"));
+        src.add(new ClickLabel("ARP Kirishima"));
+        src.add(new ClickLabel("ARP Haruna"));
+        src.add(new ClickLabel("ARP Hiei"));
+        createMultipleLabels(src, 5, "");
+
+        src.add(new JLabel("Tier VI"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier VII"));
+        src.add(new ClickLabel("ARP Myoko"));
+        src.add(new ClickLabel("ARP Haguro"));
+        src.add(new ClickLabel("ARP Ashigara"));
+        src.add(new ClickLabel("ARP Nachi"));
+        createMultipleLabels(src, 5, "");
+
+        src.add(new JLabel("Tier VIII"));
+        src.add(new ClickLabel("ARP Takao"));
+        createMultipleLabels(src, 8, "");
+
+        src.add(new JLabel("Tier IX"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier X"));
     }
 
-    private void createJapan() {
-        this.add(new JLabel(""));
-        this.add(new JLabel("Aircraft Carrier"));
-        this.add(new JLabel("Battleship"));
-        this.add(new JLabel("Cruiser"));
-        this.add(new JLabel("Destroyer"));
-        this.add(new JLabel("Destroyer"));
-        createMultipleLabels(3, "Premium");
-        this.add(new JLabel(""));
-        this.add(new JLabel("Tier I"));
-        createMultipleLabels(2, "");
-        this.add(new ClickLabel("Hashidate"));
-        createMultipleLabels(6, "");
-        this.add(new JLabel("Tier II"));
-        createMultipleLabels(2, "");
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier III"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier IV"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier V"));
-        //this.add(new JLabel("ARP Kongo"));
-        //this.add(new JLabel("ARP Kirishima"));
-        //this.add(new JLabel("ARP Haruna"));
-        //this.add(new JLabel("ARP Hiei"));
-        createMultipleLabels(5, "");
-        this.add(new JLabel("Tier VI"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier VII"));
-        //this.add(new JLabel("ARP Myoko"));
-        //this.add(new JLabel("ARP Haguro"));
-        //this.add(new JLabel("ARP Ashigara"));
-        //this.add(new JLabel("ARP Nachi"));
-        createMultipleLabels(5, "");
-        this.add(new JLabel("Tier VIII"));
-        //this.add(new JLabel("ARP Takao"));
-        createMultipleLabels(8, "");
-        this.add(new JLabel("Tier IX"));
-        createMultipleLabels(9, "");
-        this.add(new JLabel("Tier X"));
+    private void createJapan(JComponent src) {
+        src.add(new JLabel("Japan"));
+        src.add(new JLabel("Aircraft Carrier"));
+        src.add(new JLabel("Battleship"));
+        src.add(new JLabel("Cruiser"));
+        src.add(new JLabel("Destroyer"));
+        src.add(new JLabel("Destroyer"));
+        createMultipleLabels(src, 3, "Premium");
+        src.add(new JLabel(""));
+
+        src.add(new JLabel("Tier I"));
+        createMultipleLabels(src, 2, "");
+        src.add(new ClickLabel("Hashidate"));
+        createMultipleLabels(src, 6, "");
+
+        src.add(new JLabel("Tier II"));
+        createMultipleLabels(src, 2, "");
+        src.add(new ClickLabel("Chikuma"));
+        src.add(new ClickLabel("Umikaze"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier III"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier IV"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier V"));
+        //src.add(new JLabel("ARP Kongo"));
+        //src.add(new JLabel("ARP Kirishima"));
+        //src.add(new JLabel("ARP Haruna"));
+        //src.add(new JLabel("ARP Hiei"));
+        createMultipleLabels(src, 5, "");
+
+        src.add(new JLabel("Tier VI"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier VII"));
+        //src.add(new JLabel("ARP Myoko"));
+        //src.add(new JLabel("ARP Haguro"));
+        //src.add(new JLabel("ARP Ashigara"));
+        //src.add(new JLabel("ARP Nachi"));
+        createMultipleLabels(src, 5, "");
+
+        src.add(new JLabel("Tier VIII"));
+        //src.add(new JLabel("ARP Takao"));
+        createMultipleLabels(src, 8, "");
+
+        src.add(new JLabel("Tier IX"));
+        createMultipleLabels(src, 9, "");
+
+        src.add(new JLabel("Tier X"));
     }
 
-    private void createUSA() {
-
-    }
-
-    private void createUSSR() {
-
-    }
-
-    private void createKM() {
-
-    }
-
-    private void createUK() {
-
-    }
-
-    private void createPoland() {
-
-    }
-
-    private void createAsia() {
-
-    }
-
-    private void createFrance() {
-
-    }
-
-    private void createItaly() {
+    private void createUSA(JComponent src) {
 
     }
 
-    private void createCommonwealth() {
+    private void createUSSR(JComponent src) {
 
     }
 
-    private void createMultipleLabels(int i, String s) {
+    private void createKM(JComponent src) {
+
+    }
+
+    private void createUK(JComponent src) {
+
+    }
+
+    private void createPoland(JComponent src) {
+
+    }
+
+    private void createAsia(JComponent src) {
+
+    }
+
+    private void createFrance(JComponent src) {
+
+    }
+
+    private void createItaly(JComponent src) {
+
+    }
+
+    private void createCommonwealth(JComponent src) {
+
+    }
+
+    private void createMultipleLabels(JComponent src, int i, String s) {
         for (int j = 0; j < i; j ++) {
-            this.add(new JLabel(s));
+            src.add(new JLabel(s));
         }
     }
 
-    public String getNation() {
+    String getNation() {
         return nation;
+    }
+
+    private void setNation(String nation) {
+        this.nation = nation;
     }
 }
